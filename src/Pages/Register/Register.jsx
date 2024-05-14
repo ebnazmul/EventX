8e58ba7cc3db51d1075f248a92f638a81cc7cef0
imnazmul <nazmulislam2322@gmail.com>
2024-05-14 12:32:57 +0600

    fix: navigate on register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
 import { AuthContexts } from "../../Contexts/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { Helmet } from "react-helmet-async";
 
 const Register = () => {
-  const { emailPasswordRegister, continueWithGoogle, updateUserProfile, iUpdate, setIUpdate } = useContext(AuthContexts);
+  const {
+    emailPasswordRegister,
+    continueWithGoogle,
+    updateUserProfile,
+    iUpdate,
+    setIUpdate,
+  } = useContext(AuthContexts);
+
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -14,37 +22,44 @@ const Register = () => {
     emailPasswordRegister(form.email.value, form.password.value)
       .then(() => {
         updateUserProfile(form.name.value, form.photoURL.value)
-        .then(()=> {
-            toast.success("Registered successfully")
-            setIUpdate(!iUpdate)
-        })
-        .catch(()=> toast.error("Something went wrong"))
+          .then(() => {
+            toast.success("Registered successfully");
+            setIUpdate(!iUpdate);
+            navigate("/")
+          })
+          .catch(() => toast.error("Something went wrong"));
       })
       .catch((err) => {
-        toast.error("Something went wrong!")
-        console.log(err)
-    });
+        toast.error("Something went wrong!");
+        console.log(err);
+      });
   };
 
   const handleGoogle = () => {
     continueWithGoogle()
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err))
+      .then(() => {
+        toast.success("Login successfully")
+        navigate("/")
+      })
+      .catch(() => toast.error("Something went wrong!"));
   };
 
   return (
     <div className="max-w-screen-2xl mx-auto">
-      <Helmet><title>Register - EventX</title></Helmet>
+      <Helmet>
+        <title>Register - EventX</title>
+      </Helmet>
       <div className="border w-fit px-10 py-10 mx-auto my-32">
         <h2 className="text-center font-bold text-xl mb-4">Register</h2>
         <form onSubmit={handleLogin}>
-        <div>
+          <div>
             <h3 className="mb-2">Name</h3>
             <input
               name="name"
               type="text"
               className="px-2 py-1 border border-gray-400 outline-none"
-              placeholder="Enter you name..." required
+              placeholder="Enter you name..."
+              required
             />
           </div>
           <div>
@@ -53,7 +68,8 @@ const Register = () => {
               name="email"
               type="email"
               className="px-2 py-1 border border-gray-400 outline-none"
-              placeholder="Enter you email..." required
+              placeholder="Enter you email..."
+              required
             />
           </div>
           <div>
@@ -62,7 +78,8 @@ const Register = () => {
               name="password"
               type="password"
               className="px-2 py-1 border border-gray-400 outline-none"
-              placeholder="Enter you password..." required
+              placeholder="Enter you password..."
+              required
             />
           </div>
           <div>
@@ -71,7 +88,8 @@ const Register = () => {
               name="photoURL"
               type="text"
               className="px-2 py-1 border border-gray-400 outline-none"
-              placeholder="Enter you photos url..." required
+              placeholder="Enter you photos url..."
+              required
             />
           </div>
           <p className="text-sm my-2">
